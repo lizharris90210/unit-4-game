dca5a4513754f45d339940a732222f712d47c744
Liz Harris <lizharris90210@gmail.com>
2019-08-27 16:49:37 -0400

    javascript begun, css buttons styled

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+        /* <title>Crystals Collector Game</title>
+
+        <div class="container">
+            <div class="container-fluid">Crystal Gems Collector</div>
+        </div>
+        <div class="container" id="instructions">
+            <p>Four Crystal Gems are displayed.</p>
+            <p>A number has been chosen as your target.<p>
+            <p>Click on a Crystal Gem to add specific points to your score counter. </p>
+            <p>You win when your score matches the target number.</p> 
+            <p>You lose when you go over the target number.</p>
+        </div>
+        <div class="container" id="randomNumber"></div>
+        <div class="container" id="winsLosses"></div>
+        <div class="container" id="crystals"></div>
+        <div class="container" id="totalScore">Total Score:</div>
+        <div class="container" id="scoreNumber"></div> */
+
+var randomNumber= document.getElementById("randomNumber").innerHTML =
+Math.floor(Math.random() * 120) +19;
+
+// var winsLosses= ;
+
+// var scoreNumber= ;
+
