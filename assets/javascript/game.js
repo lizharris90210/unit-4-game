908c77820c29dccd3baa53c699e9d9985df6541c
Liz Harris <lizharris90210@gmail.com>
2019-08-28 11:50:25 -0400

    css styling, js onclicks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,17 @@
-        /* <title>Crystals Collector Game</title>
-
-        <div class="container">
-            <div class="container-fluid">Crystal Gems Collector</div>
-        </div>
-        <div class="container" id="instructions">
-            <p>Four Crystal Gems are displayed.</p>
-            <p>A number has been chosen as your target.<p>
-            <p>Click on a Crystal Gem to add specific points to your score counter. </p>
-            <p>You win when your score matches the target number.</p> 
-            <p>You lose when you go over the target number.</p>
-        </div>
-        <div class="container" id="randomNumber"></div>
-        <div class="container" id="winsLosses"></div>
-        <div class="container" id="crystals"></div>
-        <div class="container" id="totalScore">Total Score:</div>
-        <div class="container" id="scoreNumber"></div> */
-
-var randomNumber= document.getElementById("randomNumber").innerHTML =
-Math.floor(Math.random() * 120) +19;
-
+// returns a random integer from 19 to 120
+var randomTarget = getRndInteger(19, 120);
+console.log(getRndInteger(19, 120))
+// assigns gem random integer
+var randomGem = getRndInteger(1, 12);
+console.log(getRndInteger(1, 12))
 // var winsLosses= ;
 
 // var scoreNumber= ;
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+    target.on(click)= document.getElementById(randomTarget).innerHTML = randomTarget;
+}
+
+
 
